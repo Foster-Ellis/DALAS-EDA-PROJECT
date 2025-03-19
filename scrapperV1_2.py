6ecc51eb2b4de76cf63cddd4a6804c663d4db95c
balam0o <isaacbalam2011@hotmail.com>
2025-03-19 11:55:24 +0100

    Modified the scrapper and stores the values of inventions in two csvs

--- scrapperV1_2.py
+++ scrapperV1_2.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+
 url = "https://en.wikipedia.org/wiki/List_of_inventors"
 
 response = requests.get(url)
@@ -37,5 +39,32 @@ del inventors_list_general[0:28]
 del inventors_list_general[1073:]
 del inventors_list[341:]
 
-print(inventors_list)
-print(inventors_list_general)
+
+
+def separar_y_guardar(variable_lista, archivo_csv):
+    with open(archivo_csv, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Year"]+["Name of invention"]) 
+        
+        for item in variable_lista:
+            partes = item.split(":", 1) 
+            writer.writerow([partes[0]]+[partes[1]]) 
+
+def separar_y_guardar2(variable_lista, archivo_csv):
+    with open(archivo_csv, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Year"]+["Inventions"]) 
+        
+        for item in variable_lista:
+            partes = item.split(",", 1) 
+            if len(partes)==2:
+                writer.writerow([partes[0]]+[partes[1]]) 
+            else:
+                writer.writerow(partes) 
+
+separar_y_guardar(inventors_list, "inventors_list.csv")
+separar_y_guardar2(inventors_list_general, "inventors_list_general.csv")
+
+
+#print(inventors_list[0])
+#print(inventors_list_general[0])
